0e9c639d483f92274a686a762b6d502d565c0a4a
Giacomo Sassaro <giacomo.sassaro@gmail.com>
2025-02-24 12:06:41 +0100

    Update expression.dart

--- packages/flutter_survey_js/lib/ui/elements/expression.dart
+++ packages/flutter_survey_js/lib/ui/elements/expression.dart
@@ -4,6 +4,7 @@ import 'package:flutter_survey_js/ui/form_control.dart';
 import 'package:flutter_survey_js/ui/survey_configuration.dart';
 import 'package:flutter_survey_js/ui/survey_widget.dart';
 import 'package:flutter_survey_js_model/flutter_survey_js_model.dart' as s;
+import 'package:flutter_survey_js/ui/element_node.dart';
 
 Widget expressionBuilder(BuildContext context, s.Elementbase element,
     {ElementConfiguration? configuration}) {
@@ -14,27 +15,69 @@ Widget expressionBuilder(BuildContext context, s.Elementbase element,
   ).wrapQuestionTitle(context, e, configuration: configuration);
 }
 
-class ExpressionWidget extends StatelessWidget {
+class ExpressionWidget extends StatefulWidget {
   final s.Expression element;
 
   const ExpressionWidget({Key? key, required this.element}) : super(key: key);
+
+  @override
+  _ExpressionWidgetState createState() => _ExpressionWidgetState();
+}
+
+class _ExpressionWidgetState extends State<ExpressionWidget> {
+  late ElementNode node;
+
+  @override
+  void didChangeDependencies() {
+    node = SurveyProvider.of(context)
+        .rootNode
+        .findByElement(element: widget.element)!;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final node =
-        SurveyProvider.of(context).rootNode.findByElement(element: element)!;
-
     final effectiveDecoration = const InputDecoration()
         .applyDefaults(Theme.of(context).inputDecorationTheme);
     final decoration = effectiveDecoration.copyWith(
-        fillColor: Colors.amber.shade50,
-        errorText: getErrorTextFromFormControl(context, node.control!));
-
+      fillColor: Colors.amber.shade50,
+      errorText: getErrorTextFromFormControl(context, node.control!),
+    );
     if (node.expressionValue != node.control!.value) {
       node.control!.value = node.expressionValue;
     }
+
     return InputDecorator(
       decoration: decoration,
       child: Text(node.expressionValue?.toString() ?? ""),
     );
   }
+
+  @override
+  void dispose() {
+    node.control?.value = null;
+    super.dispose();
+  }
 }
+
+// class ExpressionWidget extends StatelessWidget {
+//   final s.Expression element;
+
+//   const ExpressionWidget({Key? key, required this.element}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     final effectiveDecoration = const InputDecoration()
+//         .applyDefaults(Theme.of(context).inputDecorationTheme);
+//     final decoration = effectiveDecoration.copyWith(
+//         fillColor: Colors.amber.shade50,
+//         errorText: getErrorTextFromFormControl(context, node.control!));
+
+//     // if (node.expressionValue != node.control!.value) {
+//     //   node.control!.value = node.expressionValue;
+//     // }
+//     return InputDecorator(
+//       decoration: decoration,
+//       child: Text(node.expressionValue?.toString() ?? ""),
+//     );
+//   }
+// }
